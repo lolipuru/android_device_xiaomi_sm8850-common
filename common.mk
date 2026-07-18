5132cafe892d5dab18f8fdae28e2165cfc6ad889
lolipuru <banstellazz@gmail.com>
2026-07-18 17:06:18 +0000

    sm8850-common: initial overlay for xiaomi 8850
    from myron OS3.0.306.0.WPMCNXM
    
    Change-Id: Ic1c418fca85d92125e4edc4bb9c79e3fe19f26ed
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- common.mk
+++ common.mk
@@ -207,6 +207,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Overlay
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlayXiaomi \
+    FrameworkResOverlayXiaomi \
+    SettingsOverlayXiaomi \
+    SystemUIOverlayXiaomi \
+    WifiOverlayXiaomi
+
 # Pagesize
 PRODUCT_CHECK_PREBUILT_MAX_PAGE_SIZE := false
 
