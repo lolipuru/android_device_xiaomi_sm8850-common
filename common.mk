e7e1c460b2e33a3fbc6aa32fa90b0995504f7f33
lolipuru <banstellazz@gmail.com>
2026-07-21 21:34:58 +0000

    sm8850-common: commonized uwb permissions
    target device guard isn't realiable anymore so better commonized it
    
    Change-Id: I452a98b5bcf537ccadcea241337d31a89934642e
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- common.mk
+++ common.mk
@@ -329,12 +329,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
-ifeq ($(filter pandora popsicle,$(TARGET_DEVICE)),)
-# UWB
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.uwb.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.uwb.xml
-endif
-
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
